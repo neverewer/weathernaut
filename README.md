73ede5796d25b98f617f8af4ec1333a810b955ba
neverewer <67904854+neverewer@users.noreply.github.com>
2023-11-08 18:40:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# weathernaut
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/weathernaut/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.weathernaut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
